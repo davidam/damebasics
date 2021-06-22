f441db7fc60ef94a59a0c1bec5744634cc3f1bbb
David Arroyo Menéndez <davidam@gmail.com>
2021-06-22 17:59:45 +0200

    src/calculator.py: fix PEP8

--- damebasics/src/calculator.py
+++ damebasics/src/calculator.py
@@ -25,12 +25,16 @@
 class Calculator(object):
     def add(self, x, y):
         return x+y
+
     def sub(self, x, y):
         return x-y
+
     def prod(self, x, y):
         return x*y
+
     def div(self, x, y):
         return x/y
+
     def prodUsingAdd(self, x, y):
         r = 0
         for i in range(0, x):
