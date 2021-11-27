3d4d1a14baafa699d2f24c6126444f256b9bcf37
David Arroyo Menéndez <davidam@gmail.com>
2021-11-27 13:19:11 +0100

    tests/test_collections.py: more about chainmap

--- damebasics/tests/test_collections.py
+++ damebasics/tests/test_collections.py
@@ -66,5 +66,8 @@ class TddInPythonExample(unittest.TestCase):
         d1 = {'a': 1, 'b': 2}
         d2 = {'c': 3, 'd': 4}
         d3 = {'e': 5, 'f': 6}
+        d4 = { 'g' : 5 }
         c = ChainMap(d1, d2, d3)
-        print(c, ChainMap({'a': 1, 'b': 2}, {'c': 3, 'd': 4}, {'e': 5, 'f': 6}))
+        self.assertEqual(c, ChainMap({'a': 1, 'b': 2}, {'c': 3, 'd': 4}, {'e': 5, 'f': 6}))
+        c = c.new_child(d4)
+        self.assertEqual(c, ChainMap({'a': 1, 'b': 2}, {'c': 3, 'd': 4}, {'e': 5, 'f': 6}, { 'g' : 5}))
