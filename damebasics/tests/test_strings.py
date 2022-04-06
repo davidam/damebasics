197f569ba533668e02ed1411d0077403fec43b74
David Arroyo Menéndez <davidam@gmail.com>
2022-04-06 13:29:33 +0200

    tests/test_strings.py: learning to do tests of the print method

--- damebasics/tests/test_strings.py
+++ damebasics/tests/test_strings.py
@@ -23,10 +23,43 @@
 
 import unittest
 import re
+import subprocess
+import io
+import sys
+
 from pprint import pprint
 
 
+class _AssertStdoutContext:
+
+    def __init__(self, testcase, expected):
+        self.testcase = testcase
+        self.expected = expected
+        self.captured = io.StringIO()
+
+    def __enter__(self):
+        sys.stdout = self.captured
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        sys.stdout = sys.__stdout__
+        captured = self.captured.getvalue()
+        self.testcase.assertEqual(captured, self.expected)
+
+
 class TddInPythonExample(unittest.TestCase):
+
+    def assertStdout(self, expected_output):
+        return _AssertStdoutContext(self, expected_output)
+
+    # as a bonus, this syntactical sugar becomes possible:
+    def assertPrints(self, *expected_output):
+        expected_output = "\n".join(expected_output) + "\n"
+        return _AssertStdoutContext(self, expected_output)
+
+    def test_print1(self):
+       with self.assertStdout("test\n"):
+          print("test")
 
     def test_string_check_type_method_returns_correct_result(self):
         doublestr = "str1str2"
